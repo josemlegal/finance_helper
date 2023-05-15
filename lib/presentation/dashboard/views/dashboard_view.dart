8ba769ba8edbeda4732d4d304382640cb4a77ebe
josemlegal <josemlegal97@gmail.com>
2023-05-14 21:07:22 -0400

    Add dashboard view

---
+++ lib/presentation/dashboard/views/dashboard_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatelessWidget {
+  static const String name = '/dashboard';
+
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
