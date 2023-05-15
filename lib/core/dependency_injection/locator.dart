282ef33b8616735de6e5789e447f7a4fb0be4eb2
josemlegal <josemlegal97@gmail.com>
2023-05-14 21:06:41 -0400

    Add locator

---
+++ lib/core/dependency_injection/locator.dart
@@ -0,0 +1,15 @@
+import 'package:finance_helper/core/services/http_service.dart';
+import 'package:get_it/get_it.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+final GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton<HttpService>(
+    () => HttpService(),
+  );
+
+  locator.registerLazySingleton<SnackbarService>(
+    () => SnackbarService(),
+  );
+}
