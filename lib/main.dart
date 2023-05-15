4912519fea06c75d25523153f0bad4b869bede1e
josemlegal <josemlegal97@gmail.com>
2023-05-14 21:06:34 -0400

    Add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
+import 'package:finance_helper/core/dependency_injection/locator.dart';
+import 'package:finance_helper/core/router/router.dart';
+import 'package:finance_helper/core/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'presentation/settings/settings_controller.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
+  setupLocator();
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  Widget build(BuildContext context, ref) {
+    final AppTheme appTheme = ref.watch(themeNotifierProvider);
+
+    return MaterialApp.router(
+      themeMode: appTheme.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      title: 'Finance Helper',
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: appTheme.getTheme(),
     );
   }
 }
