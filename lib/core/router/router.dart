dc24bb096f9ab026b16dbc2c8206c310f03797ce
josemlegal <josemlegal97@gmail.com>
2023-05-15 21:32:30 -0400

    Add more routes

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,13 +1,28 @@
-import 'package:finance_helper/presentation/dashboard/views/dashboard_view.dart';
+import 'package:finance_helper/presentation/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/dashboard',
+  initialLocation: '/home-view',
   routes: [
     GoRoute(
-      path: '/dashboard',
+      path: '/dashboard-view',
       name: DashboardView.name,
       builder: (context, state) => const DashboardView(),
+    ),
+    GoRoute(
+      path: '/home-view',
+      name: HomeView.name,
+      builder: (context, state) => const HomeView(),
+    ),
+    GoRoute(
+      path: '/transactions-view',
+      name: TransactionsView.name,
+      builder: (context, state) => const TransactionsView(),
+    ),
+    GoRoute(
+      path: '/settings-view',
+      name: SettingsView.name,
+      builder: (context, state) => const SettingsView(),
     ),
   ],
 );
