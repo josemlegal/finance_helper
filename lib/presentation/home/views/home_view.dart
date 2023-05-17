08e07d1f3db7886607e46a7cb4e62e93f6fb5e2e
josemlegal <josemlegal97@gmail.com>
2023-05-17 11:22:09 -0400

    Remove the appMenuList map

--- lib/presentation/home/views/home_view.dart
+++ lib/presentation/home/views/home_view.dart
@@ -1,7 +1,5 @@
-import 'package:finance_helper/core/menu/menu_list.dart';
 import 'package:finance_helper/presentation/widgets/side_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class HomeView extends StatelessWidget {
   static const String name = 'home-view';
@@ -16,62 +14,11 @@ class HomeView extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Finance Helper'),
         ),
-        body: const _HomeView(),
+        body: const Center(
+          child: Text('Home View'),
+        ),
         drawer: SideMenu(
           scaffoldKey: scaffoldKey,
         ));
-  }
-}
-
-class _HomeView extends StatelessWidget {
-  const _HomeView();
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: appMenuItems.length,
-      itemBuilder: (context, index) {
-        final menuItem = appMenuItems[index];
-        return _CustomListTile(menuItem: menuItem);
-      },
-    );
-  }
-}
-
-class _CustomListTile extends StatelessWidget {
-  const _CustomListTile({
-    required this.menuItem,
-  });
-
-  final MenuItem menuItem;
-
-  @override
-  Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-
-    return ListTile(
-      leading: Icon(
-        menuItem.icon,
-        color: colors.primary,
-      ),
-      title: Text(menuItem.title),
-      subtitle: Text(menuItem.subTitle),
-      trailing: Icon(
-        Icons.arrow_forward_ios_rounded,
-        color: colors.primary,
-      ),
-      splashColor: colors.primary.withOpacity(0.2),
-      onTap: () {
-        // Navigator.of(context).push(
-        //   MaterialPageRoute(
-        //     builder: (context) => const ButtonsView(),
-        //   ),
-        // );
-
-        // Navigator.of(context).pushNamed(menuItem.link);
-
-        context.push(menuItem.link);
-      },
-    );
   }
 }
