fece032552a966b371798d9bc172c0a2ba564a3a
josemlegal <josemlegal97@gmail.com>
2023-05-19 19:09:35 -0400

    Add constant

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,16 @@
+import 'package:finance_helper/presentation/onboarding/views/onboarding_view.dart';
+
+final slides = <SlidesInfo>[
+  SlidesInfo(
+      'Controla tus movimientos',
+      'Con Finance Helper podes controlar tus ingresos y egresos de una manera rapida e intuitiva.',
+      'assets/images/transaction.png'),
+  SlidesInfo(
+      'Presupuestos mensuales',
+      'Te ayudamos a mantener tus finanzas alineadas a tu presupuesto, para que puedas cumplir tus metas y objetivos.',
+      'assets/images/chart.png'),
+  SlidesInfo(
+      'Ahorra de manera inteligente',
+      'Recompensate por tu esfuerzo y date el gusto con lo que quieras.',
+      'assets/images/piggy.png')
+];
