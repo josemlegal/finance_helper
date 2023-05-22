d056b587035fb0d5b5a86137ac11d0a571c8aa86
josemlegal <josemlegal97@gmail.com>
2023-05-21 20:44:29 -0400

    Update Tx Model

--- lib/domain/models/transaction_model.dart
+++ lib/domain/models/transaction_model.dart
@@ -1,37 +1,53 @@
-import 'package:flutter/material.dart';
-
 class Transaction {
+  final int id;
+  final String description;
+  final double amount;
+  final String? userId;
+  final String? categoryId;
+  final String? paymentMethodId;
+
   Transaction({
     required this.id,
-    required this.category,
-    required this.descrption,
+    required this.description,
     required this.amount,
-    required this.icon,
-    required this.createAt,
+    this.userId,
+    this.categoryId,
+    this.paymentMethodId,
   });
-
-  final int id;
-  final String category;
-  final String descrption;
-  final double amount;
-  final IconData icon;
-  final DateTime createAt;
 
   factory Transaction.fromJson(Map<String, dynamic> json) => Transaction(
         id: json['id'],
-        category: json['title'],
-        descrption: json['subtitle'],
+        description: json['description'],
         amount: json['amount'],
-        icon: json['icon'],
-        createAt: json['createdAt'],
+        userId: json['user_id'],
+        categoryId: json['category_id'],
+        paymentMethodId: json['payment_method_id'],
       );
 
   Map<String, dynamic> toJson() => {
         'id': id,
-        'title': category,
-        'subtitle': descrption,
+        'description': description,
         'amount': amount,
-        'icon': icon,
-        'createdAt': createAt,
+        'user_id': userId,
+        'category_id': categoryId,
+        'payment_method_id': paymentMethodId,
       };
+
+  Transaction copyWith({
+    int? id,
+    String? description,
+    double? amount,
+    String? userId,
+    String? categoryId,
+    String? paymentMethodId,
+  }) {
+    return Transaction(
+      id: id ?? this.id,
+      description: description ?? this.description,
+      amount: amount ?? this.amount,
+      userId: userId ?? this.userId,
+      categoryId: categoryId ?? this.categoryId,
+      paymentMethodId: paymentMethodId ?? this.paymentMethodId,
+    );
+  }
 }
