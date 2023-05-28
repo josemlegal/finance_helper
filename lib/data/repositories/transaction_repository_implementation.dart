e498a9f35f2808184c48fd369b637773b3fa394a
josemlegal <josemlegal97@gmail.com>
2023-05-27 23:22:08 -0400

    Remove unnecesary print

--- lib/data/repositories/transaction_repository_implementation.dart
+++ lib/data/repositories/transaction_repository_implementation.dart
@@ -17,10 +17,7 @@ class TransactionRepositoryImplementation implements TransactionRepository {
   @override
   Future<List<Transaction>> getTransactions() {
     return _call(() async {
-      print('una lina antea la consulta http');
-      final response = await _httpService.dio.get(
-          "https://e254-2803-2a00-9-bfdf-9cc6-8f6d-8eb1-1aac.ngrok-free.app/api/transaction");
-      print('Me ejecute en el implementation');
+      final response = await _httpService.dio.get("/transaction");
 
       return (response.data as List)
           .map((transactions) => Transaction.fromJson(transactions))
