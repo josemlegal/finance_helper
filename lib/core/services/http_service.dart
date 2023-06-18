af5d54b621a74dcfb77aa8c6392ba49dc2c63f24
josemlegal <josemlegal97@gmail.com>
2023-06-17 22:19:57 -0400

    Add some debug prints

--- lib/core/services/http_service.dart
+++ lib/core/services/http_service.dart
@@ -3,24 +3,24 @@ import 'package:dio/dio.dart';
 class HttpService {
   final _dio = Dio();
   String baseUrl =
-      "https://57c5-2803-2a00-9-bfdf-d9e1-f9cf-1b28-1f6f.ngrok-free.app/api";
+      "https://9a46-2803-2a00-9-bfdf-8131-3436-9ca5-ec42.ngrok-free.app/api";
 
   HttpService() {
     _dio.options.baseUrl = baseUrl;
 
     _dio.interceptors.add(InterceptorsWrapper(
       onRequest: (options, handler) {
-        // print('REQUEST[${options.method}] => PATH: ${options.path}');
+        print('REQUEST[${options.method}] => PATH: ${options.path}');
         return handler.next(options);
       },
       onResponse: (response, handler) {
-        // print(
-        //     'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+        print(
+            'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
         return handler.next(response);
       },
       onError: (DioError err, handler) {
-        // print(
-        //     'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}');
+        print(
+            'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}');
         return handler.next(err);
       },
     ));
